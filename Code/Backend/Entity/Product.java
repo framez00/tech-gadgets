e2a47feb62a2502e526eaa1f2f9222540cd74e35
Fahim Ramez <fahim.ramez@icloud.com>
2025-03-02 17:29:54 -0800

    Product class, getters and setters added, finished?

--- Code/Backend/Entity/Product.java
+++ Code/Backend/Entity/Product.java
@@ -12,4 +12,39 @@ public class Product{
 		this.price = price;
 		this.quantity = quantity;
 	}
+
+	public String getProductID(){
+		return productID;
+	}
+	public void setProductID(String productID){
+		this.productID = productID;
+	}
+
+	public String getProductName(){
+		return productName;
+	}
+	public void setProductName(String productName){
+		this.productName = productName;
+	}
+
+	public String getDescription(){
+		return description;
+	}
+	public void setDescription(String description){
+		this.description = description;
+	}
+
+	public double price(){
+		return price;
+	}
+	public void setPrice(double price){
+		this.price = price;
+	}
+
+	public int getQuantity(){
+		return quantity;
+	}
+	public void setQuantity(int quantity){
+		this.quantity = quantity;
+	}
 }
