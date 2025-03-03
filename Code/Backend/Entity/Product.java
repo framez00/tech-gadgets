574ef812ad4655e665cb0975d1d0a06daa86f799
Fahim Ramez <fahim.ramez@icloud.com>
2025-03-02 17:18:12 -0800

    Started the product class, added attributes and constructor

--- Code/Backend/Entity/Product.java
+++ Code/Backend/Entity/Product.java
@@ -1,3 +1,15 @@
 public class Product{
-	
+	private String productID;
+	private String productName;
+	private String description;
+	private double price;
+	private int quantity;
+
+	public Product(String productID, String productName, String description, double price, int quantity){
+		this.productID = productID;
+		this.productName = productName;
+		this.description = description;
+		this.price = price;
+		this.quantity = quantity;
+	}
 }
