ef4361bf8c0c68b14b53acdefefa734b2400df93
Gabriel020501 <LopezGabriel020501@hotmail.com>
2025-04-02 19:53:59 -0700

    CartController.java

--- Code/Backend/CartController.java
+++ Code/Backend/CartController.java
@@ -1,28 +1,33 @@
-import java.util.HashMap;
-import java.util.Map;
 import java.util.ArrayList;
+import java.util.List;
 
 public class CartController {
-    private Map<Integer, Cart> carts;
+    private List<Cart> carts;
 
     public CartController() {
-        this.carts = new HashMap<>();
+        this.carts = new ArrayList<>();
     }
 
     public void createCart(int cartID, int customerID) {
-        if (!carts.containsKey(cartID)) {
-            carts.put(cartID, new Cart(cartID, customerID, 0, new Product[]{}));
-        } else {
-            throw new IllegalArgumentException("Cart with this ID already exists.");
+        for (Cart cart : carts) {
+            if (cart.getCartID() == cartID) {
+                throw new IllegalArgumentException("Cart with this ID already exists.");
+            }
         }
+        carts.add(new Cart(cartID, customerID, 0, new Product[]{}));
     }
 
     public Cart getCart(int cartID) {
-        return carts.get(cartID);
+        for (Cart cart : carts) {
+            if (cart.getCartID() == cartID) {
+                return cart;
+            }
+        }
+        return null; // Return null if no cart is found
     }
 
     public void addProductToCart(int cartID, Product product) {
-        Cart cart = carts.get(cartID);
+        Cart cart = getCart(cartID);
         if (cart != null) {
             cart.addProduct(product);
         } else {
@@ -31,7 +36,7 @@ public class CartController {
     }
 
     public void removeProductFromCart(int cartID, Product product) {
-        Cart cart = carts.get(cartID);
+        Cart cart = getCart(cartID);
         if (cart != null) {
             cart.removeProduct(product);
         } else {
@@ -40,32 +45,5 @@ public class CartController {
     }
 
     public float getCartTotalPrice(int cartID) {
-        Cart cart = carts.get(cartID);
-        if (cart != null) {
-            return cart.getTotalPrice();
-        } else {
-            throw new IllegalArgumentException("Cart not found.");
-        }
-    }
-
-    public void listCartProducts(int cartID) {
-        Cart cart = carts.get(cartID);
-        if (cart != null) {
-            for (Product product : cart.getListProducts()) {
-                System.out.println(product.getName() + " - $" + product.getPrice());
-            }
-        } else {
-            throw new IllegalArgumentException("Cart not found.");
-        }
-    }
-
-    public void clearCart(int cartID) {
-        Cart cart = carts.get(cartID);
-        if (cart != null) {
-            cart.setListProducts(new ArrayList<>());
-            cart.setTotalPrice(0);
-        } else {
-            throw new IllegalArgumentException("Cart not found.");
-        }
-    }
-}
+        Cart cart = getCart(cartID);
+        if (cart != null)
