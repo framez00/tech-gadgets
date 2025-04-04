ebdf77a1cba642951949931e638b26f359c4dadf
Fahim Ramez <fahim.ramez@icloud.com>
2025-04-03 20:30:50 -0700

    fixed bug in database

--- src/database.sql
+++ src/database.sql
@@ -1,1 +1,9 @@
-CREATE DATABASE database;
+CREATE DATABASE ecommerce;
+
+CREATE TABLE customer(
+    user_id INT,
+    user_name VARCHAR(15),
+    email VARCHAR(30),
+    dob DATE,
+    user_address VARCHAR(100) 
+);
