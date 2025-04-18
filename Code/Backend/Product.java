b0ae7046eeb0e7209767eb6dd74266f80b5c537a
Fahim Ramez <fahim.ramez@icloud.com>
2025-04-17 18:09:41 -0700

    fixed the product files

--- Code/Backend/Product.java
+++ Code/Backend/Product.java
@@ -5,10 +5,9 @@ public class Product {
 	private double price;
 	private int quantity;
 
-	public Product(String productID, String productName, String description, double price, int quantity) {
+	public Product(String productID, String productName, double price, int quantity) {
 		this.productID = productID;
 		this.productName = productName;
-		this.description = description;
 		this.price = price;
 		this.quantity = quantity;
 	}
