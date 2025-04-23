7544fb8ad442415552763d1f19a7b9d5ffa89896
Gabriel020501 <LopezGabriel020501@hotmail.com>
2025-04-23 13:42:59 -0700

    Update Product.java

--- Code/Backend/Product.java
+++ Code/Backend/Product.java
@@ -19,52 +19,36 @@
  *    - No algorithms are used
  */
 public class Product {
-	private String productID;
-	private String productName;
-	private double price;
-	private int quantity;
+    private String productID;
+    private String productName;
+    private String category;
+    private double price;
 
-	public Product(String productID, String productName, double price, int quantity) {
-		this.productID = productID;
-		this.productName = productName;
-		this.price = price;
-		this.quantity = quantity;
-	}
+    public Product(String productID, String productName, String category, double price) {
+        this.productID = productID;
+        this.productName = productName;
+        this.category = category;
+        this.price = price;
+    }
 
-	public String getProductID() {
-		return productID;
-	}
+    public String getProductID() {
+        return productID;
+    }
 
-	public void setProductID(String productID) {
-		this.productID = productID;
-	}
+    public String getProductName() {
+        return productName;
+    }
 
-	public String getProductName() {
-		return productName;
-	}
+    public String getCategory() {
+        return category;
+    }
 
-	public void setProductName(String productName) {
-		this.productName = productName;
-	}
+    public double getPrice() {
+        return price;
+    }
 
-	public double getPrice() {
-		return price;
-	}
-
-	public void setPrice(double price) {
-		this.price = price;
-	}
-
-	public int getQuantity() {
-		return quantity;
-	}
-
-	public void setQuantity(int quantity) {
-		this.quantity = quantity;
-	}
-
-	@Override
-	public String toString() {
-    	return productID + " : " + productName + " : " + price + " : " + quantity;
-	}
+    @Override
+    public String toString() {
+        return productID + " : " + productName + " : " + category + " : $" + price;
+    }
 }
