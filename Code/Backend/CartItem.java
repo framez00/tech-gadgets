d07cfb61ed686a63049ac0eb80b116a130e86178
Gabriel020501 <LopezGabriel020501@hotmail.com>
2025-04-23 13:42:03 -0700

    Update CartItem.java

--- Code/Backend/CartItem.java
+++ Code/Backend/CartItem.java
@@ -12,26 +12,17 @@
  * Data Structures:
  * - Product: Represents the product.
  */
-package com.infinitytech.ecommerce.model;
-
 public class CartItem {
     private Product product;
-    private int quantity;
 
-    public CartItem() {}
-
-    public CartItem(Product product, int quantity) {
+    public CartItem(Product product) {
         this.product = product;
-        this.quantity = quantity;
     }
 
     public Product getProduct() { return product; }
     public void setProduct(Product product) { this.product = product; }
 
-    public int getQuantity() { return quantity; }
-    public void setQuantity(int quantity) { this.quantity = quantity; }
-
     public double getTotalPrice() {
-        return product.getPrice() * quantity;
+        return product.getPrice();
     }
 }
